cf6a583b9a740d7b6964d5bb926e27aed21e9c47
Brandon <brandoncastillo.09@gmail.com>
2024-07-15 13:39:37 -0300

    title ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./app.css";
-import title from "./assets/logo.png";
+import title from "./assets/yugioh-title.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,11 +8,11 @@ function App() {
   return (
     <main>
       <div className="title">
-        <p>Give me a </p>
+        <p>GIVE ME A </p>
         <figure>
           <img src={title} alt="title" />
         </figure>
-        <p> Card!</p>
+        <p> CARD</p>
       </div>
     </main>
   );
