19d48e2307bf4187bc4d119cfda441de9cd430c5
Brandon <brandoncastillo.09@gmail.com>
2024-07-15 20:44:43 -0300

    analytics add

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
